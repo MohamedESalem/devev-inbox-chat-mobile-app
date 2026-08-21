1c07844178555012d55f81c6e1406502fa80287e
Mohamed Essam Salem <mohamed.e.salem@protonmail.com>
2026-08-21 21:53:31 +0300

    Format search list refs

--- src/screens/search/hooks/useSearchScreen.ts
+++ src/screens/search/hooks/useSearchScreen.ts
@@ -45,10 +45,7 @@ export function useSearchScreen() {
   );
 
   const [listRefs] = useState(() => {
-    const refs = {} as Record<
-      SearchSectionType,
-      React.RefObject<FlashListRef<SearchItem> | null>
-    >;
+    const refs = {} as Record<SearchSectionType, React.RefObject<FlashListRef<SearchItem> | null>>;
     SEARCH_SECTIONS.forEach(section => {
       refs[section.id] = React.createRef<FlashListRef<SearchItem>>();
     });
